ce83bb7eaed6147cd1315f7e46ad4d497f1bb9ec
ALANA-PC\Alana <alanamnetto@hotmail.com>
2017-06-17 23:47:21 -0300

    Adicionado Menu e Inicio da Fase 1

---
+++ Assets/Scripts/FuncoesGenericas.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FuncoesGenericas : MonoBehaviour {
+    public static FuncoesGenericas instance;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);   
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void ChangeScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+}
---
+++ Assets/Scripts/PlayBtn.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayBtn : MonoBehaviour,IPointerClickHandler {
+    
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (this.gameObject.name == "Button - Play")
+            FuncoesGenericas.instance.ChangeScene("Fase_1");
+        else
+            print(this.gameObject.name);
+    }
+}
